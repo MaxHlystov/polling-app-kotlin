b1a0c13e7eb361981b1b56b9a66f64cd6b2db8b1
MaxHlystov <maxvls@gmail.com>
2019-07-26 00:45:20 +0300

    Make vote repository

--- src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/repository/VoteRepositoryImpl.kt
+++ src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/repository/VoteRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package ru.fmtk.khlystov.hw_polling_app.repository
 
+import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Repository
 import ru.fmtk.khlystov.hw_polling_app.domain.Poll
 import ru.fmtk.khlystov.hw_polling_app.domain.PollItem
@@ -7,7 +8,7 @@ import ru.fmtk.khlystov.hw_polling_app.domain.User
 import ru.fmtk.khlystov.hw_polling_app.domain.Vote
 
 @Repository
-class VoteRepositoryImpl : voteRepositoryCusom {
+class VoteRepositoryImpl(private val mongoTemplate: MongoTemplate) : voteRepositoryCusom {
     override fun saveVote(user: User, poll: Poll, itemId: String): Vote {
         TODO("not implemented")
     }
