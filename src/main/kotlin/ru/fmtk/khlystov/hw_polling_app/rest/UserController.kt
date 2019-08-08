7011d46975164084196143cb9b9fec43576632ac
MaxHlystov <maxvls@gmail.com>
2019-08-08 23:16:10 +0300

    Change type of parameter of the authentication component.

--- src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/rest/UserController.kt
+++ src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/rest/UserController.kt
@@ -1,10 +1,7 @@
 package ru.fmtk.khlystov.hw_polling_app.rest
 
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import ru.fmtk.khlystov.hw_polling_app.domain.User
 import ru.fmtk.khlystov.hw_polling_app.repository.UserRepository
@@ -16,11 +13,11 @@ class UserController(private val userRepository: UserRepository) {
 
     @CrossOrigin
     @PostMapping("auth")
-    fun userAuth(@RequestBody(required = true) userName: String): UserDTO {
+    fun userAuth(@RequestParam(required = true) userName: String): UserDTO {
         val user: User = userRepository.findByName(userName)
                 ?: userRepository.save(User(userName))
         if (user.id == null) {
-            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error saving user.")
+            ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error saving user.")
         }
         return UserDTO(user)
     }
