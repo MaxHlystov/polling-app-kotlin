46077d17db73c1fec25e10826cbb1b8287654fec
MaxHlystov <maxvls@gmail.com>
2019-08-25 15:44:42 +0300

    Next attempt to do hw4

--- src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/rest/UserController.kt
+++ src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/rest/UserController.kt
@@ -1,24 +1,18 @@
 package ru.fmtk.khlystov.hw_polling_app.rest
 
 import org.springframework.http.HttpStatus
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.ReactiveAuthenticationManager
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.context.ReactiveSecurityContextHolder
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
-import reactor.core.publisher.SynchronousSink
-import reactor.core.publisher.toMono
 import ru.fmtk.khlystov.hw_polling_app.domain.User
 import ru.fmtk.khlystov.hw_polling_app.repository.UserRepository
 import ru.fmtk.khlystov.hw_polling_app.repository.getMonoHttpError
+import ru.fmtk.khlystov.hw_polling_app.repository.withUser
 import ru.fmtk.khlystov.hw_polling_app.rest.dto.UserDTO
-import ru.fmtk.khlystov.hw_polling_app.security.CustomUserDetails
 
 
 @RestController
@@ -26,13 +20,12 @@ class UserController(private val userRepository: UserRepository,
                      private val authenticationManager: ReactiveAuthenticationManager) {
 
     @CrossOrigin
-    @PostMapping("/auth")
+    @PostMapping("/submit")
     fun createUser(@RequestParam(required = true) userName: String,
                    @RequestParam(required = true) password: String,
                    @RequestParam(defaultValue = "") email: String): Mono<UserDTO> {
         return userRepository.findByName(userName)
-                .flatMap { user ->
-                    val r = user == null
+                .flatMap {
                     getMonoHttpError<User>(HttpStatus.INTERNAL_SERVER_ERROR, "Error creating user: user with such login already exists.")
                 }
                 .switchIfEmpty(userRepository.save(User(null, userName, email, password)))
@@ -43,34 +36,11 @@ class UserController(private val userRepository: UserRepository,
 
     @CrossOrigin
     @PostMapping("/login")
-    fun loginUser(@RequestParam(required = true) userName: String,
-                  @RequestParam(required = true) password: String): Mono<UserDTO> {
-        val authenticationToken = UsernamePasswordAuthenticationToken(userName, password)
-        val authentication = this.authenticationManager.authenticate(authenticationToken)
-        authentication.doOnError { throw BadCredentialsException("Bad credentials") }
-        ReactiveSecurityContextHolder.withAuthentication(authenticationToken)
-        return authentication.map { auth -> auth.principal }
-                .cast(CustomUserDetails::class.java)
-                .map(CustomUserDetails::user)
-                .map { user ->
-                    if (user.id == null) {
-                        throw BadCredentialsException("User with null id.")
-                    } else {
-                        UserDTO(user)
-                    }
-                }
-    }
-
-    @CrossOrigin
-    @PostMapping("/auth/get")
-    fun getCredentials(): Mono<UserDTO> {
-        return ReactiveSecurityContextHolder.getContext()
-                .map { securityContext -> securityContext.authentication }
-                .map { authentication -> authentication.principal }
-                .cast(CustomUserDetails::class.java)
-                .map { userDetails ->
-                    println("Polls getting for user: ${userDetails.username}")
-                    UserDTO(userDetails.user)
-                }
+    fun login(exchange: ServerWebExchange): Mono<UserDTO> {
+        return withUser()
+                .map { user -> UserDTO(user) }
+        /*.doOnNext { userDTO ->
+            //addTokenHeader(exchange.response, userDTO)
+        }*/
     }
 }
--- src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/security/SecurityConfiguration.kt
+++ src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/security/SecurityConfiguration.kt
@@ -1,23 +1,20 @@
 package ru.fmtk.khlystov.hw_polling_app.security
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.server.SecurityWebFilterChain
-import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler
 import org.springframework.security.web.server.authentication.WebFilterChainServerAuthenticationSuccessHandler
-import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter
-import ru.fmtk.khlystov.hw_polling_app.repository.UserRepository
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.reactive.CorsConfigurationSource
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite
+import reactor.core.publisher.Mono
 
 
 @Configuration
@@ -27,16 +24,20 @@ class SecurityConfiguration(private val userDetailsService: CustomUserDetailsSer
     @Bean
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         http.csrf().disable()
+                .authorizeExchange().pathMatchers(HttpMethod.OPTIONS, "**").permitAll()
+                .and()
                 .authorizeExchange().pathMatchers("/auth", "/login").permitAll()
                 .and()
                 .authorizeExchange().pathMatchers("/polls/**", "/votes/**").authenticated()
                 .and()
                 .formLogin()
-                .authenticationSuccessHandler(RedirectServerAuthenticationSuccessHandler("/auth/get"))
-                //.authenticationManager(authenticationManager)
-                //.loginPage("/login")
-                //.authenticationFailureHandler { exchange, exception -> Mono.error(exception) }
-
+                .loginPage("/login")
+                .authenticationSuccessHandler(WebFilterChainServerAuthenticationSuccessHandler())
+                .authenticationFailureHandler { _, exception -> Mono.error(exception) }
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint { _, exception -> Mono.error(exception) }
+                .accessDeniedHandler { _, exception -> Mono.error(exception) }
         return http.build()
     }
 
@@ -50,51 +51,51 @@ class SecurityConfiguration(private val userDetailsService: CustomUserDetailsSer
         return authManager
     }
 
+//    @Bean
+//    fun corsConfigurationSource(): CorsConfigurationSource {
+//        val configuration = CorsConfiguration()
+//        configuration.allowedOrigins = listOf("http://localhost:3000")
+//        configuration.allowedMethods = listOf("OPTIONS", "GET", "POST", "DELETE", "PUT")
+//        val source = UrlBasedCorsConfigurationSource()
+//        source.registerCorsConfiguration("/**", configuration)
+//        return source
+//    }
+
+//    @Bean
+//    fun corsFilter(): FilterRegistrationBean {
+//        val source = UrlBasedCorsConfigurationSource()
+//        val config = CorsConfiguration()
+//        config.allowCredentials = true
+//        config.addAllowedOrigin("*") // @Value: http://localhost:8080
+//        config.addAllowedHeader("*")
+//        config.addAllowedMethod("*")
+//        source.registerCorsConfiguration("/**", config)
+//        val bean = FilterRegistrationBean(CorsWebFilter(source))
+//        bean.setOrder(0)
+//        return bean
+//    }
+
+//    @Bean
+//    fun corsWebFilter(): CorsWebFilter {
+//        val config = CorsConfiguration()
+//        config.allowCredentials = true
+//        config.addAllowedOrigin("http://localhost:3000")
+//        config.addAllowedHeader("*")
+//        config.addAllowedMethod("*")
+//        val source = UrlBasedCorsConfigurationSource()
+//        source.registerCorsConfiguration("/**", config)
+//        return CorsWebFilter(source)
+//    }
+
     @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("*") //""http://localhost:3000")
-        configuration.allowedMethods = listOf("GET", "POST", "DELETE", "PUT")
-        val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", configuration)
-        return source
-    }
-}
+    fun corsConfigurer(): WebFluxConfigurer {
+        return object : WebFluxConfigurerComposite() {
 
-/*
-@Configuration
-@EnableReactiveMethodSecurity
-@EnableWebFluxSecurity
-class SecurityConfiguration {
-    @Bean
-    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-        http.csrf().disable()
-                .authorizeExchange().pathMatchers("/auth", "/login").permitAll()
-                .and()
-                .authorizeExchange().pathMatchers("/polls", "/votes").authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-
-        return http.build()
-    }
-
-
-    @Bean
-    open fun passwordEncoderAndMatcher(): PasswordEncoder {
-        return object : PasswordEncoder {
-            override fun encode(rawPassword: CharSequence?): String {
-                return BCryptPasswordEncoder().encode(rawPassword)
-            }
-            override fun matches(rawPassword: CharSequence?, encodedPassword: String?): Boolean {
-                return BCryptPasswordEncoder().matches(rawPassword, encodedPassword)
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("**").allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowCredentials(true)
             }
         }
     }
-
-//    @Bean
-//    fun authenticationManager(@Autowired userRepository: ReactiveUserDetailsService): ReactiveAuthenticationManager {
-//        return UserDetailsRepositoryReactiveAuthenticationManager(userRepository)
-//    }
 }
-*/
