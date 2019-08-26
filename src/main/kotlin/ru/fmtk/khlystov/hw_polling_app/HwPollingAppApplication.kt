b080126fa14726f7f97c80b942f28109de0324dd
MaxHlystov <maxvls@gmail.com>
2019-08-26 11:47:25 +0300

    Added logging when users' passwords set.

--- src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/HwPollingAppApplication.kt
+++ src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/HwPollingAppApplication.kt
@@ -1,13 +1,12 @@
 package ru.fmtk.khlystov.hw_polling_app
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.core.env.Environment
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.reactive.config.EnableWebFlux
 import reactor.core.publisher.Flux
@@ -18,6 +17,8 @@ import ru.fmtk.khlystov.hw_polling_app.repository.UserRepository
 @SpringBootApplication
 @EnableWebFlux
 class HwPollingAppApplication {
+
+    val log: Logger = LoggerFactory.getLogger(HwPollingAppApplication::class.java)
 
     @Bean
     fun start(@Autowired userRepository: UserRepository,
@@ -35,6 +36,7 @@ class HwPollingAppApplication {
                                 passwordEncoder.encode("111111"))
                     }
                     .flatMap { user -> userRepository.save(user) }
+                    .doOnNext { user -> log.info("Set \"111111\" password for user ${user.name}") }
                     .subscribe()
         }
     }
