d15f7f60d5a1dc756646ad0e241ba43e2ff7f194
MaxHlystov <maxvls@gmail.com>
2019-09-25 15:53:02 +0300

    Fixed some issues.

--- src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/repository/UserRepository.kt
+++ src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/repository/UserRepository.kt
@@ -7,10 +7,9 @@ import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
 import ru.fmtk.khlystov.hw_polling_app.domain.User
 
-@Repository
 @RepositoryRestResource(path = "admin_users")
 interface UserRepository : ReactiveMongoRepository<User, String> {
 
-    @RestResource(path = "users")
+    @RestResource(rel = "find", path = "find")
     fun findByName(name: String): Mono<User>
 }
