45003d7521c02c3394c8fcabb0c983edfb1f002e
MaxHlystov <maxvls@gmail.com>
2019-09-30 13:18:00 +0300

    Added component annotation.

---
+++ src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/actuator/TotalPollsInfoContributor.kt
@@ -0,0 +1,16 @@
+package ru.fmtk.khlystov.hw_polling_app.actuator
+
+import org.springframework.boot.actuate.info.Info
+import org.springframework.boot.actuate.info.InfoContributor
+import org.springframework.stereotype.Component
+import ru.fmtk.khlystov.hw_polling_app.repository.PollRepository
+import java.util.HashMap
+
+@Component
+class TotalPollsInfoContributor(private val pollRepository: PollRepository) : InfoContributor {
+    override fun contribute(builder: Info.Builder?) {
+        val pollsStatistics = HashMap<String, Long>()
+        pollsStatistics["total"] = pollRepository.count().block() ?: 0
+        builder?.withDetail("polls", pollsStatistics)
+    }
+}
