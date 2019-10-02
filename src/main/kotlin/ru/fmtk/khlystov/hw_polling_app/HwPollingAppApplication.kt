24a74b218a40173bd0bcf03c92140c2470b75c3c
MaxHlystov <maxvls@gmail.com>
2019-10-02 23:42:04 +0300

    Removed logging of db connection.

--- src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/HwPollingAppApplication.kt
+++ src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/HwPollingAppApplication.kt
@@ -3,7 +3,6 @@ package ru.fmtk.khlystov.hw_polling_app
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -22,18 +21,8 @@ class HwPollingAppApplication {
     @Autowired
     lateinit var updateMongoDb: UpdateMongoDb
 
-    @Value("\${spring.data.mongodb.host}")
-    lateinit var host: String
-
-    @Value("\${spring.data.mongodb.port}")
-    lateinit var port: String
-
-    @Value("\${spring.data.mongodb.database}")
-    lateinit var database: String
-
     @Bean
     fun start(): CommandLineRunner {
-        log.info("Mongo connection string: $host:$port/$database")
         return CommandLineRunner {
             updateMongoDb.update()
 
