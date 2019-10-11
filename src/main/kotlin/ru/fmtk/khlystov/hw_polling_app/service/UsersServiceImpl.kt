28b7a7899e49aed547c293f0db07babbfd3d602e
MaxHlystov <DhwU2qXsZ7M81Pm3ws>
2019-10-11 20:06:06 +0300

    Change delay for test needs.

--- src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/service/UsersServiceImpl.kt
+++ src/main/kotlin/ru/fmtk/khlystov/hw_polling_app/service/UsersServiceImpl.kt
@@ -1,46 +1,42 @@
 package ru.fmtk.khlystov.hw_polling_app.service
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.cloud.netflix.hystrix.HystrixCommands
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import ru.fmtk.khlystov.hw_polling_app.domain.User
 import ru.fmtk.khlystov.hw_polling_app.repository.UserRepository
+import java.time.Duration
+import java.util.*
 
 @Service
 class UsersServiceImpl(private val userRepository: UserRepository) : UsersService {
+    val log: Logger = LoggerFactory.getLogger(UsersServiceImpl::class.java)
 
-    override fun findAll(): Flux<User> = userRepository.findAll()
-//        HystrixCommands
-//            .from(userRepository.findAll())
-//            .fallback(Flux.just(User(null, "Empty user")))
-//            .groupName("Users")
-//            .commandName("FindAllUsers")
-//            .toFlux()
+    override fun findAll(): Flux<User> = HystrixCommands
+            .from(userRepository.findAll().withRandomDelay())
+            .fallback(Flux.just(User(null, "Empty user")))
+            .groupName("Users")
+            .commandName("FindAllUsers")
+            .toFlux()
 
     override fun findById(id: String): Mono<User> = userRepository.findById(id)
-//    HystrixCommands
-//            .from(userRepository.findById(id))
-//            .fallback(Mono.just(User(null, "Empty user")))
-//            .groupName("Users")
-//            .commandName("FindUserById")
-//            .toMono()
 
     override fun save(user: User): Mono<User> = userRepository.save(user)
-//    HystrixCommands
-//            .from(userRepository.save(user))
-//            .fallback(Mono.just(User(null, "Empty user")))
-//            .groupName("Users")
-//            .commandName("SaveUser")
-//            .toMono()
 
     override fun delete(user: User): Mono<Void> = userRepository.delete(user)
 
     override fun findByName(name: String): Mono<User> = userRepository.findByName(name)
-//    HystrixCommands
-//            .from(userRepository.findByName(name))
-//            .fallback(Mono.just(User(null, "Empty user")))
-//            .groupName("Users")
-//            .commandName("FindUserByName")
-//            .toMono()
+
+    fun <T> Flux<T>.withRandomDelay(): Flux<T> = this.flatMap { value ->
+        val newFlux = Flux.just(value)
+        val rand = Random()
+        if (rand.nextInt(3) == 0) {
+            newFlux.delaySubscription(Duration.ofSeconds(3))
+            log.info("Delay in 3 seconds for value $value.")
+        }
+        newFlux
+    }
 }
